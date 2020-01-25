bf93d81aaee57d0965caf1dc61d5d781d26b8977
Widiyanto9876 <40840576+Widiyanto9876@users.noreply.github.com>
2020-01-25 20:15:22 +0700

    Update storage.go

--- storage.go
+++ storage.go
@@ -5,7 +5,7 @@ import (
 	"log")
 
 func connect() *sql.DB {
-	// 2 variabel sekaligus kembaliannya *db,eror (nil = null)
+
 	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/latihan")
 
 	if err != nil {
